b71018df73aca9c75d92e237d45a556576da6925
Mayookh <mayookh@beatly.ai>
2025-08-26 10:48:39 +0530

    cors updated

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,7 +1,7 @@
 
 export const API_URL={
-    amService: "http://localhost:8080/auth-role-Am",
-    spService: "http://localhost:8080/auth-role-Sp",
-    apiService:"http://localhost:8080/auth-role",
-    userService: "http://localhost:8080/user",
+    amService: "http://localhost:7001/auth-role-Am",
+    spService: "http://localhost:7001/auth-role-Sp",
+    apiService:"http://localhost:7001/auth-role",
+    userService: "http://localhost:7001/user",
 }
